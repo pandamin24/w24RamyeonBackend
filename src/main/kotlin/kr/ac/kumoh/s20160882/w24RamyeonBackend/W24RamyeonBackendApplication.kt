111e390236a1d5dd115fa5a394e8f6b1ec161614
pandamin24 <gbdkaltb@naver.com>
2024-12-13 16:26:34 +0900

    🎉 Term Project Backend 시작

---
+++ src/main/kotlin/kr/ac/kumoh/s20160882/w24RamyeonBackend/W24RamyeonBackendApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.s20160882.w24RamyeonBackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class W24RamyeonBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<W24RamyeonBackendApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/s20160882/w24RamyeonBackend/W24RamyeonBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20160882.w24RamyeonBackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24RamyeonBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
